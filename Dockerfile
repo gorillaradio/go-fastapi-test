65dc44c617cc7dab8f135bafadbdc060d00a2ed7
Sebastiano Pitruzzello <seb@MBPdiSebastiano.fritz.box>
2024-08-05 13:35:08 +0200

    Remove locale setting for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,7 @@
 # Usa l'immagine base di Python
 FROM python:3.9-slim
 
-# Installa le localizzazioni e altre dipendenze necessarie
-RUN apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/*
-
-# Configura la locale
-RUN sed -i '/it_IT.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
-
-# Imposta le variabili di ambiente per la locale
-ENV LANG it_IT.UTF-8
-ENV LANGUAGE it_IT:it
-ENV LC_ALL it_IT.UTF-8
-
-# Imposta la directory di lavoro
+# Imposta la directory di lavoro all'interno del container
 WORKDIR /app
 
 # Copia il file delle dipendenze e installale
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-import logging
-import locale
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-logger.info("Starting application")
-
-try:
-    locale.setlocale(locale.LC_TIME, 'it_IT.UTF-8')
-    logger.info("Locale set to it_IT.UTF-8")
-except locale.Error as e:
-    logger.error(f"Locale setting failed: {e}")
 
 app = FastAPI()
 
@@ -26,32 +13,6 @@ app.add_middleware(
     allow_headers=["*"],  # Permette tutti gli headers
 )
 
-# Imposta la localizzazione italiana per i nomi dei mesi
-locale.setlocale(locale.LC_TIME, 'it_IT.UTF-8')
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/formatdata/{data}")
-def format_data(data: str):
-    try:
-        # Converte la stringa in un oggetto datetime
-        data_obj = datetime.strptime(data, "%d-%m-%Y")
-        
-        # Formatta la data come richiesto
-        mese = data_obj.strftime("%B")  # Nome completo del mese
-        giorno = data_obj.strftime("%d")
-        anno = data_obj.strftime("%Y")
-        
-        # Crea il testo di risposta
-        risposta = f"La data che hai inserito è il giorno {giorno} {mese.capitalize()} dell'anno {anno}."
-        
-        return {"messaggio": risposta}
-    except ValueError:
-        # Se la data non è nel formato corretto, solleva un'eccezione
-        raise HTTPException(status_code=400, detail="Formato data non valido. Usa DD-MM-YYYY.")
+    return {"message": "Hello World"}
